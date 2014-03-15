947e8549557c0758e8b61ac1a6c56befb444218a
David Birch <birch.dave@gmail.com>
2014-03-15 12:05:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AFLT4::Application.config.secret_key_base = '24380a80eee0baedf4b091b295e17cc156db1252d6c749a5bf2db0ae8eb766cc1162c3aab83abdf09f2f0f3beb0231a208611dd8a00bfd862624035c6916a5a0'
